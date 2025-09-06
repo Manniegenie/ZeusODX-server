8dde9609f9b8e157a6538743beed31968148445a
Manniegenie <Manuelwurld@gmail.com>
2025-09-06 16:27:19 +0100

    -=45

--- routes/NIN.js
+++ routes/NIN.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const SmileIDNINService = require('../services/SmileIDNINService');
+const SmileIDNINService = require('../services/SmileIDService');
 const EmailVerificationService = require('../services/VerifiedEmail');
 const logger = require('../utils/logger');
 
