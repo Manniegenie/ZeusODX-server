61743f3fae499cf646fa471590159d6c75418454
Manniegenie <Manuelwurld@gmail.com>
2025-11-08 17:29:05 +0100

    93003

--- services/createwalletaddress.js
+++ services/createwalletaddress.js
@@ -40,7 +40,7 @@ class ObiexService {
         {
           headers: {
             'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
           }
         }
       );
