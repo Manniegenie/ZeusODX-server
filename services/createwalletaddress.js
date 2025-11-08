2f13884ca7bc2ad304167e50f0726d3a24c0bdb3
Manniegenie <Manuelwurld@gmail.com>
2025-11-08 17:23:41 +0100

    9003003

--- services/createwalletaddress.js
+++ services/createwalletaddress.js
@@ -34,7 +34,16 @@ class ObiexService {
     try {
       logger.info('Creating deposit address:', payload);
 
-      const response = await this.axiosClient.post(path, payload);
+      const response = await this.axiosClient.post(
+        path,
+        payload,
+        {
+          headers: {
+            'User-Agent':
+              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+          }
+        }
+      );
 
       const address = response.data?.data?.value;
 
